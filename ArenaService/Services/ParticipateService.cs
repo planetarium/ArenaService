162f0b421c2a2b4422c7003aff87902576011568
Atralupus <me@atralupus.com>
2025-02-01 01:20:27 +0900

    participate

--- ArenaService/Services/ParticipateService.cs
+++ ArenaService/Services/ParticipateService.cs
@@ -21,16 +21,22 @@ public class ParticipateService : IParticipateService
     private readonly IParticipantRepository _participantRepo;
     private readonly IUserRepository _userRepo;
     private readonly IRankingRepository _rankingRepo;
+    private readonly IGroupRankingRepository _groupRankingRepo;
+    private readonly IClanRankingRepository _clanRankingRepo;
 
     public ParticipateService(
         IParticipantRepository participantRepo,
         IUserRepository userRepo,
-        IRankingRepository rankingRepo
+        IRankingRepository rankingRepo,
+        IGroupRankingRepository groupRankingRepo,
+        IClanRankingRepository clanRankingRepo
     )
     {
         _participantRepo = participantRepo;
         _userRepo = userRepo;
         _rankingRepo = rankingRepo;
+        _groupRankingRepo = groupRankingRepo;
+        _clanRankingRepo = clanRankingRepo;
     }
 
     public async Task<Participant> ParticipateAsync(
@@ -40,6 +46,9 @@ public class ParticipateService : IParticipateService
         Func<IQueryable<Participant>, IQueryable<Participant>>? includeQuery = null
     )
     {
+        includeQuery ??= q => q;
+        includeQuery = q => includeQuery(q).Include(p => p.User);
+
         var existingParticipant = await _participantRepo.GetParticipantAsync(
             seasonId,
             avatarAddress,
@@ -66,6 +75,37 @@ public class ParticipateService : IParticipateService
             roundId + 1,
             participant.Score
         );
+
+        await _groupRankingRepo.UpdateScoreAsync(
+            participant.AvatarAddress,
+            seasonId,
+            roundId,
+            0,
+            participant.Score
+        );
+        await _groupRankingRepo.UpdateScoreAsync(
+            participant.AvatarAddress,
+            seasonId,
+            roundId + 1,
+            0,
+            participant.Score
+        );
+
+        if (participant.User.ClanId is not null)
+        {
+            await _clanRankingRepo.UpdateScoreAsync(
+                participant.User.ClanId.Value,
+                seasonId,
+                roundId,
+                participant.Score
+            );
+            await _clanRankingRepo.UpdateScoreAsync(
+                participant.User.ClanId.Value,
+                seasonId,
+                roundId + 1,
+                participant.Score
+            );
+        }
 
         return participant;
     }
