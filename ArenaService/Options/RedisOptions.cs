b62535f19166e1fe9c1b0f6a582e780201783f33
Atralupus <me@atralupus.com>
2025-03-10 17:12:28 +0900

    Redis auth

--- ArenaService/Options/RedisOptions.cs
+++ ArenaService/Options/RedisOptions.cs
@@ -5,6 +5,8 @@ public class RedisOptions
     public const string SectionName = "Redis";
     public string Host { get; set; } = "127.0.0.1";
     public string Port { get; set; } = "6379";
+    public string Username { get; set; } = "";
+    public string Password { get; set; } = "";
     public string HangfirePrefix { get; set; } = "arena_hangfire:";
     public int HangfireDbNumber { get; set; } = 0;
     public int RankingDbNumber { get; set; } = 1;
