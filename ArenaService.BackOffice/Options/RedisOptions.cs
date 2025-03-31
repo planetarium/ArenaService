a3b84195f2bb70d9117f4efd8fcd5d9ccdbdf711
Atralupus <me@atralupus.com>
2025-04-01 00:59:16 +0900

    Backoffice redis auth

--- ArenaService.BackOffice/Options/RedisOptions.cs
+++ ArenaService.BackOffice/Options/RedisOptions.cs
@@ -5,5 +5,7 @@ public class RedisOptions
     public const string SectionName = "Redis";
     public string Host { get; set; } = "127.0.0.1";
     public string Port { get; set; } = "6379";
+    public string Username { get; set; } = "";
+    public string Password { get; set; } = "";
     public int RankingDbNumber { get; set; } = 1;
 }
